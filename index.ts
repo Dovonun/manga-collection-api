07e9c24135cd0c4adf8cc24afeea042138740bca
dovonun <31408384+Dovonun@users.noreply.github.com>
2024-03-31 15:44:27 +0200

    initial prisma pg setup

---
+++ index.ts
@@ -0,0 +1,5 @@
+const host = Bun.env.DB_HOST;
+const user = Bun.env.DB_USER;
+const database = Bun.env.DB;
+
+console.log(host, user, database);
---
+++ prisma/migrations/20240331130113_init/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "books" (
+    "isbn" TEXT NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("isbn")
+);
